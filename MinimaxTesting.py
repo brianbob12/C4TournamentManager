8b24dce605747f34b598b01a744839386b170852
Cyrus Singer <japaneserhino@gmail.com>
2022-03-10 14:02:41 +0000

    Update MinimaxTesting.py

--- MinimaxTesting.py
+++ MinimaxTesting.py
@@ -1,5 +1,5 @@
 from Game import Game
-
+from copy import deepcopy
 
 def minimax(gameState,depth,player):
     length=12
@@ -9,8 +9,12 @@ def minimax(gameState,depth,player):
         return 500*gameState.winningPlayer
     children=[]
     for i in range(length):
-        newState=gameState.makeMove(player,i)
-        children.append(minimax(newState,depth-1,player*-1))
+        newState=deepcopy(gameState)
+        placed,win=newState.makeMove(player,i)
+        if not placed:
+            continue
+        else:
+            children.append(minimax(newState,depth-1,player*-1))
     if player==-1:
         return(min(children))
     else:
